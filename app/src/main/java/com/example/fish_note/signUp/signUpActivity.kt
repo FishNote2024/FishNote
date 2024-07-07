202498b36c8239dbf8aaefa2080faf912eb3fd71
haram22 <kimhr9889@handong.ac.kr>
2024-07-07 21:14:48 +0900

    [Fix] add hint text in textfield

--- app/src/main/java/com/example/fish_note/signUp/signUpActivity.kt
+++ app/src/main/java/com/example/fish_note/signUp/signUpActivity.kt
@@ -1,13 +1,18 @@
 package com.example.fish_note.signUp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.fish_note.R
+import com.example.fish_note.databinding.ActivityLoginBinding
+import com.example.fish_note.databinding.ActivitySignUpBinding
 
 class signUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +21,10 @@ class signUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        binding.yearNextBtn.setOnClickListener {
+            Toast.makeText(this, "clicked by binding", Toast.LENGTH_LONG).show()
         }
     }
 }
